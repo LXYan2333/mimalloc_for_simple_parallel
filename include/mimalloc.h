2ebea550c246b0ea12cdb01cb07f73223ae04c89
LXYan2333 <z00823823@126.com>
2025-07-03 00:23:07 +0800

    export some symbol for shared library simple_parallel

--- include/mimalloc.h
+++ include/mimalloc.h
@@ -187,11 +187,11 @@ mi_decl_nodiscard mi_decl_export void* mi_realloc_aligned_at(void* p, size_t new
 struct mi_heap_s;
 typedef struct mi_heap_s mi_heap_t;
 
-mi_decl_nodiscard mi_decl_export mi_heap_t* mi_heap_new(void);
+mi_decl_nodiscard __attribute__((visibility("default"))) mi_heap_t *mi_heap_new(void);
 mi_decl_export void       mi_heap_delete(mi_heap_t* heap);
 mi_decl_export void       mi_heap_destroy(mi_heap_t* heap);
-mi_decl_export mi_heap_t* mi_heap_set_default(mi_heap_t* heap);
-mi_decl_export mi_heap_t* mi_heap_get_default(void);
+__attribute__((visibility("default"))) mi_heap_t *mi_heap_set_default(mi_heap_t *heap);
+__attribute__((visibility("default"))) mi_heap_t *mi_heap_get_default(void);
 mi_decl_export mi_heap_t* mi_heap_get_backing(void);
 mi_decl_export void       mi_heap_collect(mi_heap_t* heap, bool force) mi_attr_noexcept;
 
@@ -263,7 +263,7 @@ typedef struct mi_heap_area_s {
 
 typedef bool (mi_cdecl mi_block_visit_fun)(const mi_heap_t* heap, const mi_heap_area_t* area, void* block, size_t block_size, void* arg);
 
-mi_decl_export bool mi_heap_visit_blocks(const mi_heap_t* heap, bool visit_all_blocks, mi_block_visit_fun* visitor, void* arg);
+__attribute__((visibility("default"))) bool mi_heap_visit_blocks(const mi_heap_t *heap, bool visit_all_blocks, mi_block_visit_fun *visitor, void *arg);
 
 // Experimental
 mi_decl_nodiscard mi_decl_export bool mi_is_in_heap_region(const void* p) mi_attr_noexcept;
